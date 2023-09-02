ba2fdcef8aeb98bec95781cddc6bd3decc21a18d
choelzli <128326139+choelzli@users.noreply.github.com>
2023-09-02 12:29:42 -0500

    no front end, removed config var note

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-//* Don't forget to add Atlas connection string as a Config Var
-
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
 
